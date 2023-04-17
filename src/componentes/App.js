bb3693fb061369b46671f5de46c971dc4ca06080
Rey-86 <reygranhacristian@gmail.com>
2023-04-17 20:29:24 +0200

    crear tienda

---
+++ src/componentes/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Producto from "./producto";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Probando</h2>
+        <Producto/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
